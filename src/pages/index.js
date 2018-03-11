a8b34c03f2dfb14470f24355fbfe59932532add9
David Harting <david@emplify.com>
2018-03-10 22:05:06 -0500

    add age

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import timediff from 'timediff'
+import pluralize from 'pluralize'
 import andiImageUrl from '../assets/andi.jpg'
 
 const Birthday = () => (
   <p>Andi was born on December 25, 2017</p>
 )
 
+const Age = () => {
+  const age = timediff(new Date(2017, 12, 25), new Date())
+  return (
+    <h1>
+      {age.months} {pluralize('month', age.months)},&nbsp;
+      {age.weeks} {pluralize('week', age.weeks)},
+      and {age.days} {pluralize('day', age.days)} old
+    </h1>
+  )
+}
+
 const IndexPage = () => (
   <div>
     <img src={andiImageUrl} />
+    <Age />
     <Birthday />
   </div>
 )
