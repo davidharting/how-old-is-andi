0f9e3e53798084ef70e7cc4870394e85fe87e527
David Harting <david@emplify.com>
2018-03-10 21:41:49 -0500

    add picture of andi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import andiImageUrl from '../assets/andi.jpg'
 
 const IndexPage = () => (
   <div>
+    <img src={andiImageUrl} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
